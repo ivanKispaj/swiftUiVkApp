68b2d7676cb77d3da43d61df37e355cdfcc85021
Ivan Konishchev <kispaj@mail.ru>
2022-10-18 07:47:49 +0300

    Initial Commit

---
+++ swiftUiVkApp/swiftUiVkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUiVkAppApp.swift
+//  swiftUiVkApp
+//
+//  Created by Ivan Konishchev on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftUiVkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
