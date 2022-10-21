6b1b3da1c29120e052ce4a9fd72c223fcdd8e35b
Ivan Konishchev <kispaj@mail.ru>
2022-10-21 07:07:34 +0300

    Create homework

--- swiftUiVkApp/ContentView.swift
+++ swiftUiVkApp/ContentView.swift
@@ -6,17 +6,86 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @State private var center: CGFloat = UIScreen.main.bounds.height / 2
+    @State private var email: String = ""
+    @State private var login = ""
+    @State private var password = ""
+    
+    private let keyboardIsOnPublisher = Publishers.Merge( NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification)
+        .map { _ in true }, NotificationCenter.default.publisher(for:
+                                                                    UIResponder.keyboardWillHideNotification) .map { _ in false }
+    )
+        .removeDuplicates()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-           
-        }.padding()
-        
+        ZStack {
+            // backgroundImage
+            Image("VKlogo")
+                .resizable()
+                .scaledToFit()
+                .edgesIgnoringSafeArea(.all)
+                .opacity(0.5)
+            // ScrollView
+            ScrollView(.vertical) {
+                
+                Spacer(minLength: self.center
+                       - 200)
+                VStack {
+                    HStack {
+                        Text("Email: ")
+                            .font(Font.system(size: 20,design: .monospaced))
+                            .frame(width: 150)
+                        //  Spacer(minLength: 40)
+                        TextField("Enter Your Email", text: $email)
+                            .background(Color(UIColor.lightGray))
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .frame(width: 200)
+                    }
+                    .padding(.top, 40)
+                    
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Text("Password: ")
+                            .font(Font.system(size: 20,design: .monospaced))
+                            .frame(width: 150)
+                        
+                        // Spacer()
+                        SecureField("Enter Your password", text: $password)
+                            .background(Color(UIColor.lightGray))
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .frame(width: 200)
+                    }
+                    .padding(25)
+                    
+                    HStack{
+                        Button(action: { print("Hello") }) {
+                            Text("Войти")
+                        }
+                        .padding(.top, 8)
+                        .padding(.trailing, 40)
+                        .padding(.leading, 40)
+                        .padding(.bottom, 8)
+                        .disabled(login.isEmpty || password.isEmpty)
+                        .background(Color(UIColor.white))
+                        .onReceive(keyboardIsOnPublisher) { isKeyboardOn in withAnimation(Animation.easeInOut(duration: 0.5)) {
+                            
+                        }
+                            
+                        }
+                    }
+                    .padding(.bottom, 20)
+                }
+                .frame(width: UIScreen.main.bounds.width)
+                .background(Color(UIColor.systemGray).opacity(0.4))
+                
+                
+            }
+        }
     }
 }
 
@@ -25,3 +94,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
