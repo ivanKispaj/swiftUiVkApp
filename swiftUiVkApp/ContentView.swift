ab4ead93a7e11a015f3ea9cb18348eb51c5895a2
Ivan Konishchev <kispaj@mail.ru>
2022-10-23 20:04:45 +0300

    Add change: Apgrade: fix bag for any orientation screen

--- swiftUiVkApp/ContentView.swift
+++ swiftUiVkApp/ContentView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 import Combine
 
-struct ContentView: View {
+struct LoginScreen: View {
     @State private var center: CGFloat = UIScreen.main.bounds.height / 2
     @State private var email: String = ""
-    @State private var login = ""
     @State private var password = ""
     @State private var isTrue = true
-    
+    @State private var should: CGFloat = 130
+    var size: CGSize
     private let keyboardIsOnPublisher = Publishers.Merge( NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification)
         .map { _ in true }, NotificationCenter.default.publisher(for:
                                                                     UIResponder.keyboardWillHideNotification) .map { _ in false }
@@ -22,89 +22,128 @@ struct ContentView: View {
         .removeDuplicates()
     
     var body: some View {
-        let _ = print("Update ContentView")
+        
         ZStack {
+            
             // backgroundImage
-            Image("VKlogo")
-                .resizable()
-                .scaledToFit()
-                .edgesIgnoringSafeArea(.all)
-                .opacity(0.5)
-            // ScrollView
-            ScrollView(.vertical) {
-                
-                Spacer(minLength: self.center
-                       - 200)
-                VStack {
-                    if isTrue {
-                    Text("Войти") .font(.largeTitle) .padding(.top, 32)
-                    } else {
-                        Text("Вошли") .font(.largeTitle) .padding(.top, 32)
-                    }
-                    HStack {
-                        Text("Email: ")
-                            .font(Font.system(size: 20,design: .monospaced))
-                            .frame(width: 150)
-                        TextField("Enter Your Email", text: $email)
-                            .background(Color(UIColor.lightGray))
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .frame(width: 200)
-                    }
-                    .padding(.top, 40)
-                    
-                    Spacer()
-                    
-                    HStack {
-                        Text("Password: ")
-                            .font(Font.system(size: 20,design: .monospaced))
-                            .frame(width: 150)
-                        
-                        SecureField("Enter Your password", text: $password)
-                            .background(Color(UIColor.lightGray))
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .frame(width: 200)
-                    }
-                    .padding(25)
-                    
-                    HStack{
-                        Button(action: {
-                            isTrue.toggle()
-                        })
-                        {
-                            Text("Войти")
-                        }
-                       
-                        .padding(.top, 8)
-                        .padding(.trailing, 40)
-                        .padding(.leading, 40)
-                        .padding(.bottom, 8)
-                        .disabled(email.isEmpty || password.isEmpty)
-                        .background(Color(UIColor.white))
-                        .onReceive(keyboardIsOnPublisher) { isKeyboardOn in withAnimation(Animation.easeInOut(duration: 0.5)) {
-                            
-                        }
-                            
-                        }
-                    }
-                    .padding(.bottom, 20)
-                }
-                .frame(width: UIScreen.main.bounds.width)
-                .background(Color(UIColor.systemGray).opacity(0.4))
-                
+            GeometryReader { geometry in
+                Image("VKlogo")
+                    .frame(width: geometry.size.width,height: geometry.size.height)
+                    .opacity(0.6)
                 
             }
-           
+            
+            // ScrollView
+            
+            ScrollView(showsIndicators: false) {
+                
+                VStack {
+                    
+                    VStack {
+                        
+                        if isTrue {
+                            Text("Войти") .font(.largeTitle)
+                                .padding(.top, 15)
+                        } else {
+                            Text("Вошли") .font(.largeTitle)
+                                .padding(.top, 15)
+                        }
+                        
+                        VStack {
+                            HStack {
+                                Text("Email: ")
+                                    .font(Font.system(size: 20,design: .monospaced))
+                                    .frame(width: 150)
+                                TextField("Enter Your Email", text: $email)
+                                    .background(Color(UIColor.lightGray))
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .frame(width: 200)
+                                
+                            }
+                            
+                            Spacer()
+                            
+                            HStack {
+                                Text("Password: ")
+                                    .font(Font.system(size: 20,design: .monospaced))
+                                    .frame(width: 150)
+                                
+                                SecureField("Enter Your password", text: $password)
+                                    .background(Color(UIColor.lightGray))
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .frame(width: 200)
+                                    .accessibilityRespondsToUserInteraction()
+                                
+                            }
+                        }
+                        .frame(minWidth: size.width)
+                        
+                        Spacer(minLength: 20)
+                        
+                        Button(action: {
+                            self.isTrue.toggle()
+                            
+                        }) {
+                            Text("Войти")
+                        }
+                        .padding(.bottom, 20)
+                        .disabled(email.isEmpty || password.isEmpty)
+                        .tint(  .primary)
+                        
+                        .buttonStyle(.bordered)
+                       .buttonBorderShape(.roundedRectangle(radius: 10))
+                    }
+                    .background(Color.gray.opacity(0.3))
+                    .onReceive(keyboardIsOnPublisher) { isKeyboardOn in
+                        withAnimation(Animation.easeInOut(duration: 0.5)) {
+                            if isKeyboardOn {
+                                self.should = 180
+                            } else {
+                                self.should = 130
+                            }
+                        }
+                    }
+                }
+                
+                .padding(.top, size.height / 2 - self.should)
+            }
+            .onTapGesture {
+                 UIApplication.shared.endEditing()
+            
+             }
+            
         }
-    
+        
     }
+    
 }
 
+
+// чтобы передать размер екрана
+struct _ContentView: View {
+    var body: some View {
+        GeometryReader { proxy in
+            LoginScreen(size: proxy.size)
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+    
+    
+}
+
+// для удаления клавиатуры по тапу в свободном месте
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    
+}
+
+// Для превью
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        _ContentView()
+        
     }
 }
-
-
-
 
--- swiftUiVkApp/swiftUiVkAppApp.swift
+++ swiftUiVkApp/swiftUiVkAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct swiftUiVkAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            _ContentView()
         }
     }
 }
