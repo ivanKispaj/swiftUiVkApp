7fa0458eb3bad0d257cce13a540ca75b570c7319
Ivan Konishchev <kispaj@mail.ru>
2022-11-11 17:14:12 +0300

    Add change: Clean build

--- swiftUiVkApp/FriendsScene/FriendsScene.swift
+++ swiftUiVkApp/FriendsScene/FriendsScene.swift
@@ -11,24 +11,9 @@ import UIKit
 
 struct FriendsScene: View {
     
-    @State var isOnScreen: Int = 0
     @EnvironmentObject var userData: UserRegistrationData
-    var transition: Int {
-        if isOnScreen == 0 {
-            return withAnimation(.spring(response: 1, dampingFraction: 0.5, blendDuration: 100.0)) {
-                0
-            }
-        }
-        return withAnimation (.spring(response: 0.1, dampingFraction: 0.1, blendDuration: 100.0)) {
-            10
-        }
-    }
-    
     @ObservedObject var viewModel: FriendsViewModel
-    @State var nextDestination: String = ""
-    @State var selection = false
     @State private var isloadedFriend = false
-  //  private var userId = UserDefaults.standard.string(forKey: self.userData.userId)
     
     init(viewModel: FriendsViewModel) {
         self.viewModel = viewModel
@@ -77,9 +62,7 @@ struct FriendsScene: View {
         })
         
         .onAppear {
-            if isloadedFriend {
-                
-            } else {
+            if !isloadedFriend {
                 viewModel.internetConnection.loadFriends(for: userData.userId,token: userData.token) { response in
                     self.isloadedFriend.toggle()
                     DispatchQueue.main.async {
@@ -89,33 +72,7 @@ struct FriendsScene: View {
             }
         }
     }
-    private func textToImage(drawText text: String, inImage image: UIImage, atPoint point: CGPoint) -> UIImage {
-        let textColor = UIColor.blue
-        let textFont = UIFont(name: "Helvetica Bold", size: 10)!
-        
-        let scale = UIScreen.main.scale
-        var size = image.size
-        size.height = 60
-        size.width = 60
-        
-        UIGraphicsBeginImageContextWithOptions(size, false, 0)
-        
-        let textFontAttributes = [
-            NSAttributedString.Key.font: textFont,
-            NSAttributedString.Key.foregroundColor: textColor,
-        ] as [NSAttributedString.Key : Any]
-        image.draw(in: CGRect(origin: CGPoint(x: 20, y: 15), size: image.size))
-        
-        
-        let rect = CGRect(origin: point, size: size)
-        
-        text.draw(in: rect, withAttributes: textFontAttributes)
-        
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return newImage!
-    }
+
 }
 
 
@@ -133,28 +90,4 @@ struct LabelStyleCustom: View {
     }
 }
 
-
-
-extension View {
-    @ViewBuilder
-    func swipeAction(@ViewBuilder _ content: @escaping () -> some View) -> some View {
-        VStack(spacing: 0) {
-            Image(systemName: "trash")
-                .font(.system(size: 18.0))
-                .foregroundColor(.white)
-                .frame(width: 60, alignment: .center)
-                .frame(maxHeight: .infinity) // <====== HERE
-            
-            
-            Text("Delite")
-                .foregroundColor(.black)
-                .font(.system(size: 12))
-                .padding(.bottom, 10)
-            
-            //  .animation(.spring(), value: isOnScreen )
-            
-        }
-        .background(.red)
-    }
-}
 
